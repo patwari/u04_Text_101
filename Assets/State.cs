0e8a75e2880d50dfcf2061b9a1053841d27a4107
Rahul Patwari <rahulpatwari233@gmail.com>
2019-01-28 01:29:48 +0530

    Add option to create state in right click menu
    It makes it easier to initialize objects
    Udemy: s03-24

--- Assets/State.cs
+++ Assets/State.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu (menuName = "State")]
 public class State : ScriptableObject {
-
+	[TextArea (10, 14)][SerializeField] string storyText;
 }
