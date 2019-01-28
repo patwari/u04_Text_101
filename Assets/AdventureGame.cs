245fd4c538d3f4787362757f2a987882ab2a41a4
Rahul Patwari <rahulpatwari233@gmail.com>
2019-01-28 21:46:07 +0530

    Switch text based on player input
    Udemy: s03-26

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -8,21 +8,25 @@ public class AdventureGame : MonoBehaviour {
 	[SerializeField] Text textComponent;
 	[SerializeField] State startingState;
 
-	string[] daysOfWeek = { "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun" };
-
 	State currentState;
 
 	// Use this for initialization
 	void Start () {
 		currentState = startingState;
 		textComponent.text = currentState.GetStateStory ();
-		foreach (string day in daysOfWeek) {
-			Debug.Log (day);
-		}
 	}
 
 	// Update is called once per frame
 	void Update () {
+		ManageState ();
+		textComponent.text = currentState.GetStateStory ();
+	}
 
+	private void ManageState () {
+		if (Input.GetKeyDown (KeyCode.Keypad1) || Input.GetKeyDown (KeyCode.Alpha1) || Input.GetKeyDown (KeyCode.DownArrow) || Input.GetKeyDown (KeyCode.LeftArrow)) {
+			currentState = currentState.GetNextStates () [0];
+		} else if (Input.GetKeyDown (KeyCode.Keypad2) || Input.GetKeyDown (KeyCode.Alpha2) || Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.RightArrow)) {
+			currentState = currentState.GetNextStates () [1];
+		}
 	}
 }
--- Assets/State.cs
+++ Assets/State.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 [CreateAssetMenu (menuName = "State")]
 public class State : ScriptableObject {
 	[TextArea (10, 14)][SerializeField] string storyText;
+	[SerializeField] State[] nextStates;
 
 	public string GetStateStory () {
 		return storyText;
+	}
+
+	public State[] GetNextStates () {
+		return nextStates;
 	}
 }
