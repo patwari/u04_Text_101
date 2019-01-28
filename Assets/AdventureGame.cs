d50bdcf43ff5d92ae3dceb6af00d063236b3ee34
Rahul Patwari <rahulpatwari233@gmail.com>
2019-01-28 21:12:16 +0530

    Assign text of starting state
    Udemy: s03-25

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -6,10 +6,14 @@ using UnityEngine.UI;
 public class AdventureGame : MonoBehaviour {
 
 	[SerializeField] Text textComponent;
+	[SerializeField] State startingState;
+
+	State currentState;
 
 	// Use this for initialization
 	void Start () {
-		textComponent.text = "I am added programmatically";
+		currentState = startingState;
+		textComponent.text = currentState.GetStateStory ();
 	}
 
 	// Update is called once per frame
--- Assets/State.cs
+++ Assets/State.cs
@@ -5,4 +5,8 @@ using UnityEngine;
 [CreateAssetMenu (menuName = "State")]
 public class State : ScriptableObject {
 	[TextArea (10, 14)][SerializeField] string storyText;
+
+	public string GetStateStory () {
+		return storyText;
+	}
 }
