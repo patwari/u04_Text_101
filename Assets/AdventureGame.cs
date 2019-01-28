f272bf17b0fd7bfedbfb479adbb9df2e6ddd5920
Rahul Patwari <rahulpatwari233@gmail.com>
2019-01-28 21:24:47 +0530

    Iterate through array to print days
    Udemy: s03-26

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -8,12 +8,17 @@ public class AdventureGame : MonoBehaviour {
 	[SerializeField] Text textComponent;
 	[SerializeField] State startingState;
 
+	string[] daysOfWeek = { "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun" };
+
 	State currentState;
 
 	// Use this for initialization
 	void Start () {
 		currentState = startingState;
 		textComponent.text = currentState.GetStateStory ();
+		foreach (string day in daysOfWeek) {
+			Debug.Log (day);
+		}
 	}
 
 	// Update is called once per frame
